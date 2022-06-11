35e39c807b254af3d91bc86b7b42ae8f5693da32
EfazAhmed <efazahmededu@gmail.com>
2022-06-11 14:28:29 -0400

    Complete About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import picture from '../assets/picture.jpg';
+
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={picture}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Efaz Ahmed</div>
+            <div className="brief_description">
+              Hello! My name is Efaz Ahmed and I am a rising junior at Baruch College, NYC. My major is Data Analytics
+              and my minor is Computer Science. I love to create Web Applications and software that involves Data Science.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
